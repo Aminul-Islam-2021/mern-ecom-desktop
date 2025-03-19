2f285c0277bca5dc5985e9de4fc89ab517bbd686
aminul-islam <aminulislamshykat@gmail.com>
2025-03-20 03:06:39 +0600

    initial setup-2

---
+++ server/src/app.js
@@ -0,0 +1,8 @@
+// import files & packages
+import express from "express";
+
+// initialize the express app
+const app = express();
+
+// export the app
+export default app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,14 @@
-console.log(`Server is running`)
+// import files and packages
+import app from "./app.js";
+import dotenv from "dotenv";
+
+// configure dotenv
+dotenv.config();
+
+// initialize the port
+const port = process.env.PORT || 3000;
+
+// listen to the port & run the server
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
